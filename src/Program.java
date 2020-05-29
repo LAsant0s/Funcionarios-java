198aaf3f7dc4ce42c6a056bfe74d0c5f681a37cd
Luan Assis <luan.assis05.santos@gmail.com>
2020-05-29 19:21:19 -0300

    Leitura dos dados

--- src/Program.java
+++ src/Program.java
@@ -1,9 +1,37 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
-
+		String nome;
+		int horasTrab;
+		double valorPorHora; 
+		char opcao;
+		
+		do {
+			System.out.print("Nome: ");
+			nome = sc.next(); 
+			
+			System.out.print("Horas trabalhadas: ");
+			horasTrab = sc.nextInt(); 
+			while(horasTrab < 0) {
+				System.out.print("Valores negativos nao sao aceitos. Insira novamante: ");
+				horasTrab = sc.nextInt();
+			}
+			
+			System.out.print("Valor por hora: ");
+			valorPorHora = sc.nextDouble(); 
+			while(valorPorHora < 0) {
+				System.out.print("Valores negativos nao sao aceitos. Insira novamente: ");
+				valorPorHora = sc.nextDouble();
+			}
+			
+			System.out.print("Digitar outro (S/N)? ");
+			opcao = sc.next().charAt(0); 
+		}while(opcao != 'N');
 	}
-
 }
